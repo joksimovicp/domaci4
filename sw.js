7a1c7721d473d7a6ee7e1e603f613e8491b56ab1
joksimovicp <70727264+joksimovicp@users.noreply.github.com>
2020-09-06 21:50:46 +0200

    initial commit

---
+++ sw.js
@@ -0,0 +1,75 @@
+const staticAssets = [
+  '/index.html',
+  '/',
+  '/css/style.css',
+  '/index.js',
+  '/cars.html',
+  '/drivers.html',
+  '/login.html',
+  '/signup.html',
+  '/js/indexScripts.js',
+  '/js/jquery-3.2.1.js',
+  '/img/antp.jpg',
+  '/img/brazil.jpg',
+  '/img/brazil1.jpg',
+  '/img/brazil2.jpg',
+  '/img/carsback.jpg',
+  '/img/enzo.jpg',
+  '/img/f1.png',
+  '/img/favicon.ico',
+  '/img/favicon1.ico',
+  '/img/formback.jpg',
+  '/img/inst.png',
+  '/img/italia.jpg',
+  '/img/kimi.jpg',
+  '/img/logo.png',
+  '/img/maurizio.jpg',
+  '/img/mexico1.jpg',
+  '/img/mexico2.jpg',
+  '/img/mexico3.jpg',
+  '/img/seb.jpg',
+  '/img/sebp.jpg',
+  '/img/sf15t1.jpg',
+  '/img/sf15t2.jpg',
+  '/img/sf16h1.jpg',
+  '/img/sf16h2.jpg',
+  '/img/sf70h1.jpg',
+  '/img/sf70h2.jpg',
+  '/img/sf2007.jpg',
+  '/img/sfbanner.jpg',
+  '/img/sumi.jpg',
+  '/img/team.jpg',
+  '/img/tradition.png',
+  '/img/twit.png',
+  '/offline.html',
+  '/css/animate.css',
+  '/css/bootstrap.css.map'
+]
+
+this.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('static-assets').then(cache => {
+      return cache.addAll(staticAssets)
+    })
+  )
+})
+
+this.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (resp) {
+      return resp || fetch(event.request).then(function (response) {
+        return caches.open('static-assets').then(function (cache) {
+          cache.put(event.request, response.clone())
+          return response
+        })
+      }).catch(() => {
+        console.log('Fetch failed; returning offline page instead.');
+        return caches.open('static-assets').then(function (cache) {
+          cache.match('offlne.html').then(function (cachedResponse) {
+            return cachedResponse
+          })
+        });
+      })
+    })
+  )
+})
